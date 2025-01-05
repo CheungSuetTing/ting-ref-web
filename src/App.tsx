fedd4a86e71e2b2d1ac0403699b263efd78bc9e7
kevin <kevinli8831@gmail.com>
2025-01-05 19:01:12 +0800

    grid container layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { Button } from "./components/ui/button";
 function App() {
   // https://www.vivolatam.com/en
   return (
-    <>
-      <div>hello world</div>
-<Button>hello world</Button>
-    </>
+    <div className="grid-container">
+      <div className="header">header</div>
+      <div className="main">main</div>
+      <div className="footer">footer</div>
+    </div>
   );
 }
 
