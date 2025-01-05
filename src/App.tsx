e54c41caf604575d8335e61525b640c6bbe4e444
kevin <kevinli8831@gmail.com>
2025-01-05 18:21:43 +0800

    add ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-// https://www.vivolatam.com/en
+  // https://www.vivolatam.com/en
   return (
     <>
       <div>hello world</div>
+<Button>hello world</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
