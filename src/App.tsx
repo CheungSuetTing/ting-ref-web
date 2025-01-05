2e3b8f443dfb876055a9a7eb864b470ef045eabb
kevin <kevinli8831@gmail.com>
2025-01-05 17:42:24 +0800

    first init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+// https://www.vivolatam.com/en
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  )
+}
+
+export default App
