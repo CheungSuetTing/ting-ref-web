bf3b4209d9e124e995f24a57d48f46c59589d28c
kevin <kevinli8831@gmail.com>
2025-01-25 18:33:15 +0800

    Footer-ing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="grid-container">
       <div className="header">
         <div>
-          <img className="logo" src="/cover.png" width={76}></img>
+          <img className="logo" src="/logo.png" width={76}></img>
         </div>
         <div className="flex items-center gap-6 text-sm">
           <div className="flex gap-1 hover:cursor-pointer">
@@ -32,25 +32,57 @@ function App() {
 
       <div className="main">main</div>
       <div className="footer">
-        <div>About Vivo Latam recommendations</div>
-        <div>
+        <div className="font-bold pb-4">About Vivo Latam recommendations</div>
+        <div className="max-w-[768px] pb-6">
           Recommendations are based on your location and search activity, such as the real estate properties you've viewed and saved and the
           filters you've used. We use this information to bring similar real estate properties to your attention.
         </div>
-        <div>
+        <div className="flex gap-8 pb-6">
           <Select>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Theme" />
+            <SelectTrigger className="min-w-[180px]">
+              <SelectValue placeholder="Real Estate" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">Light</SelectItem>
-              <SelectItem value="dark">Dark</SelectItem>
-              <SelectItem value="system">System</SelectItem>
+              <SelectItem value="light">San Salvador real estate</SelectItem>
+              <SelectItem value="dark">Colonia San Benito real estate</SelectItem>
+              <SelectItem value="system">Antiguo Cuscatlán real estate</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="min-w-[180px]">
+              <SelectValue placeholder="Rentals" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">San Salvador apartments for rent</SelectItem>
+              <SelectItem value="dark">Colonia San Benito apartments for rent</SelectItem>
+              <SelectItem value="system">Antiguo Cuscatlán apartments for rent</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="min-w-[180px]">
+              <SelectValue placeholder="Homes" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">San Salvador homes</SelectItem>
+              <SelectItem value="dark">Colonia San Benito homes</SelectItem>
+              <SelectItem value="system">Antiguo Cuscatlán homes</SelectItem>
             </SelectContent>
           </Select>
         </div>
         <hr className="w-[30%]"></hr>
-        <div>a</div>
+        <div className="w-full flex justify-between self-start pt-10">
+          <img className="logo" src="/logo.png" width={76}></img>
+          <div className="flex flex-col gap-4 items-end">
+            <div className="flex gap-8">
+              <div>About</div> <div>Blog</div> <div>Contact</div> <div>Legal</div>
+            </div>
+            <div className="flex gap-4">
+              <img src="/Icon/facebook.svg" width={24}></img>
+              <img src="/Icon/x.svg" width={24}></img>
+              <img src="/Icon/ig.svg" width={24}></img>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
