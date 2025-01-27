10b030017b6a42cc46080f839b77fbbd6bfdffc9
Ting <109614974+CheungSuetTing@users.noreply.github.com>
2025-01-27 19:24:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+     base:'/ting-ref-web/',
  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-   base:'/ting-ref-web/'
   },
 })
