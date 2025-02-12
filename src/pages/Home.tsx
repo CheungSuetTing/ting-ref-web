96799dc137bf991a1563a22df8fcd3c97e5c1376
kevin <kevinli8831@gmail.com>
2025-02-12 22:06:45 +0800

    bababa

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import SearchIcon from "../assets/search.svg?react";
 import "../App.css";
 import img1Url from "../assets/img1.jpg";
 import img2Url from "../assets/img2.jpg";
@@ -11,7 +12,10 @@ function Home() {
       <div className="w-1/2 pr-[30px]">
         <div className="font-medium text-7xl/[1.2] text-[#f8fafc] pb-[40px]">El Salvador homes, apartments, land & more</div>
         <div className="text-slate-500 pb-[40px]">Vivo Latam makes buying and selling real estate easy.</div>
-        <Button size="lg">Search Property</Button>
+        <Button size="lg" className="bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 animate-gradient">
+          <SearchIcon className="!fill-" />
+          Search Property
+        </Button>
       </div>
       <div className="flex w-1/2 gap-6 h-fit items-center">
         <div className="flex flex-col gap-6 w-1/2">
